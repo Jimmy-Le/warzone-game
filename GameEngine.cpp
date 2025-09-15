5cb56a356afafefa5a15239488c2449a3ea4d04e
wonarii <ariellecwong@gmail.com>
2025-09-15 10:05:07 -0400

    Tried
    Messed around and attempted to write some code (didn't work)
    I don't know what I'm doing:(

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -1,1 +1,9 @@
 //Controls the flow of the game.
+
+#include <iostream>
+using namespace std;
+
+int listen(){
+    //Prompts user for command
+    cout << "Enter a command: ";
+}
--- GameEngine.h
+++ GameEngine.h
@@ -1,1 +1,29 @@
 //Header to the GameEngine.cpp file
+#include <string>
+
+
+/*
+--> Base class for status
+All other status classes will inherit from this one.
+This is an abstract class/ interface.
+*/
+class Status {
+    protected: //Accessible only in inherited classes
+    Status(); //default constructor
+    //friend bool operator ==();
+    //friend istream& operator >>(istream& ins, something);
+};
+
+/*
+--> Takes input from command line
+*/
+int listen();
+
+/*
+--> Changes the state
+Takes an int (corresponds to a state in the system)
+Switches the state pointer to a new state object
+OR
+Displays an error message
+*/
+void switchTo(int);
