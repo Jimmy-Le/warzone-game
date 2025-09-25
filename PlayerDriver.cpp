54ac3fec44b524d0bc0663ad07f948ac57f15ded
Jimmy <jimmy_le_@hotmail.com>
2025-09-24 23:34:29 -0400

    added fake orders and orderlist

--- PlayerDriver.cpp
+++ PlayerDriver.cpp
@@ -1,8 +1,17 @@
 #include "Player.h"
 
-int main(){
+
+
+void testPlayers(){
     Player abc;
     abc.toAttack();
     abc.toDefend();
     abc.issueOrder();
+    abc.issueOrder();
+    abc.issueOrder();
+    abc.printPlayer();
+}
+
+int main(){
+    testPlayers();
 }
