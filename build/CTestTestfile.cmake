868f003cce08294165e38ddca0343abc960f95cf
Nisma <nismanawaz77@gmail.com>
2025-09-28 18:42:00 -0400

    Small Changes

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/warzoneOrderList/warzone-game
-# Build directory: D:/warzoneOrderList/warzone-game/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
