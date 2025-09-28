e98f8f09d1a146c226957ca726a6bfca91ef5fac
Jimmy <jimmy_le_@hotmail.com>
2025-09-28 00:50:16 -0400

    added copy constructor, destructor, stream insertion opperator, player driver menu

--- Player.cpp
+++ Player.cpp
@@ -9,19 +9,99 @@ using namespace std;
  * TODO Update this to be the proper type
  */
 Player::Player(){
-    defendCollection = new vector<string>({"France", "Canada", "USA"});
-    attackCollection = new vector<string>({"France", "Canada", "USA"});
-    cardCollection = new vector<string>({"1", "2", "3"});
-    orderCollection = new FakeOrderList();
+    this->name = new string("John Doe");
+    this->defendCollection = new vector<string>({"France", "Canada", "USA"});
+    this->attackCollection = new vector<string>({"Russia", "Ukraine", "China"});
+    this->cardCollection = new vector<string>({"1", "2", "3"});
+    this->orderCollection = new FakeOrderList();
+}
+/***
+ * Parameterized Player Constructor,
+ * Initializes the set of territories, cards, and orderlist of a Player
+ * Gives the player a name
+ * TODO Update this to be the proper type
+ */
+Player::Player(string name) {
+    this->name = new string(name);
+    this->defendCollection = new vector<string>({"France", "Canada", "USA"});
+    this->attackCollection = new vector<string>({"Russia", "Ukraine", "China"});
+    this->cardCollection = new vector<string>({"1", "2", "3"});
+    this->orderCollection = new FakeOrderList();
 }
 
 /***
- * toDefend()
+ * Player Destructor
+ * Frees all the dynamically allocated memory inside the Player object
+ */
+Player::~Player(){
+    delete name;
+    delete defendCollection;
+    delete attackCollection;
+    delete cardCollection;
+    delete orderCollection;
+}
+/***
+ * Player Copy Constructor
+ * Creates a deep copy of the Player object
+ */
+Player::Player(const Player& other){
+    // Deep copy of defendCollection
+    defendCollection = new vector<string>(*other.defendCollection);
+    
+    // Deep copy of attackCollection
+    attackCollection = new vector<string>(*other.attackCollection);
+    
+    // Deep copy of cardCollection
+    cardCollection = new vector<string>(*other.cardCollection);
+    
+    // Deep copy of orderCollection
+    // TODO Replace with actual OrderList class
+    orderCollection = new FakeOrderList();
+    for (FakeOrder* order : other.orderCollection->orders) {
+        FakeOrder* newOrder = new FakeOrder(*order); // Assuming FakeOrder has a copy constructor
+        orderCollection->add(newOrder);
+    }
+}
+
+/***
+ * Overloading the << operator for easy printing of Player details
+ * TODO Update this to be the proper type
+ */
+ostream& operator<<(ostream& out, const Player& player) {
+    out << "Player Details:" << endl;
+
+    out << "Name: " << *player.name << endl;                                // Player's name
+
+    out << "\n-Territories to Defend: " << endl;                                      // Territories to be defended
+    for (const string& territory : *player.defendCollection) {
+        out << territory << " " <<endl;
+    }
+
+    out << "\n-Territories to Attack: " << endl;                                       // Territories to be attacked
+    for (const string& territory : *player.attackCollection) {
+        out << territory << " " << endl;
+    }
+
+    out << "\n-Cards: " << endl;
+    for (const string& card : *player.cardCollection) {                     // Cards in player's hand
+        out << card << " " << endl;
+    }
+
+    out << "\n-Orders: " << endl;
+    for (FakeOrder* order : player.orderCollection->orders) {               // Orders in player's order list
+        order->printOrder();                                              
+    }
+
+    return out;
+}
+
+/***
+ * toDefend() 
  * Returns a list of territories that are to be defended
  * TODO Arbitrarily choose which territory to be defended
  */
 vector<string>* Player::toDefend(){
-    cout << "printing Territory to defend" << endl;
+    // cout << "printing Territory to defend" << endl;
     return defendCollection;
 }
 
@@ -31,7 +111,7 @@ vector<string>* Player::toDefend(){
  * TODO Arbitrarily choose which territory to be attacked
  */
 vector<string>* Player::toAttack(){
-    cout << "printing Territory to attack" << endl;
+    // cout << "printing Territory to attack" << endl;
     return attackCollection;
 }
 
@@ -47,35 +127,6 @@ void Player::issueOrder(){
 
 }
 
-/***
- * printPlayer()
- * Prints the player's details for debugging purposes
- */
-void Player::printPlayer(){
-    cout << "Player's Territories to Defend: " << endl;
-    for (string& territory : *defendCollection) {
-        cout << territory << " ";
-    }
-    cout << endl;
-
-    cout << "Player's Territories to Attack: " << endl;
-    for (string& territory : *attackCollection) {
-        cout << territory << " ";
-    }
-    cout << endl;
-
-    cout << "Player's Cards: " << endl;
-    for (string& card : *cardCollection) {
-        cout << card << " ";
-    }
-    cout << endl;   
-
-    cout << "Player's Orders: " << endl;
-    for (FakeOrder* order : orderCollection->orders) {
-        order->printOrder();
-    }
-    cout << endl;
-}
 
 
 // ------------------------------- Fake Stuff (Delete After) --------------------------------
--- Player.h
+++ Player.h
@@ -31,14 +31,22 @@ class FakeOrderList {
 class Player{
     public:
         Player();                                                       // Player Constructor
-
+        Player(string name);                                            // Parameterized Constructor
+        ~Player();                                                      // Player Destructor
+        Player(const Player& other);                                    // Player Copy Constructor
+        
         // TODO Change string to list of territories 
         vector<string>* toDefend();                                      // Returns a list of territories to be defended
         vector<string>* toAttack();                                      // Returns a list of territories to be attacked
         void issueOrder();                                              // Create a Order object and add it to the OrderList (orderCollection)
-        void printPlayer();                                             // Print the player's details (for debugging)
+
+        friend ostream& operator << (ostream& out, const Player& player);   // Overload the << operator for easy printing of Player details
+        string getName() const { return *name; }                            // Getter for player's name
+        
 
     private:
+        string* name;
+
         // TODO change int to pointer of territories and Cards
         vector<string>* defendCollection;                                // A List of Territories the Player should Defend
         vector<string>* attackCollection;                                // A List of Territories the Player should Attack
